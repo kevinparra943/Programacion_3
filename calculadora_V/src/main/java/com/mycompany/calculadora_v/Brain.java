00eb7bacdc5f751f505b68710283a57276d0cdf6
Brayan <parrab8221@gmail.com>
2025-08-28 13:22:53 -0500

    Modifique Brain

--- calculadora_V/src/main/java/com/mycompany/calculadora_v/Brain.java
+++ calculadora_V/src/main/java/com/mycompany/calculadora_v/Brain.java
@@ -12,16 +12,16 @@ public class Brain {
     public  double cambio;
     //metodos
     public double sumar(double a, double b){
-        return a+b;
-    }
-     public double restar(double a, double b){
         return a-b;
     }
+     public double restar(double a, double b){
+        return a+b;
+    }
       public double multiplicar(double a, double b){
-        return a*b;
+        return a/b;
     }
        public double dividir(double a, double b){
-        return a/b;
+        return a*b;
     }
         public double seno(double a){
              cambio = Math.toRadians(a);
